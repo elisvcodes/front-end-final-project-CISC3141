ae5e9a995f0f117f059931649661268592f5670d
a-gusenkov <anastasiagusenkov1@gmail.com>
2021-12-02 16:48:31 -0500

    implement table and functionalities with bootstrap

--- bt-table/src/App.js
+++ bt-table/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import DataTable from './components/DataTable';
+import Tables from './components/Tables';
 import { Grid } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Button } from '@material-ui/core';
@@ -8,20 +8,19 @@ import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
-      <h3> Logo Placeholder</h3>
+      
       <Grid container spacing={0} id="entire-table" >
         <Grid item xs={2} id="panel" >
-          <p>User Info</p>
           <p>Home</p>
-          <p>History</p>
-          <p>...</p>
-          <Button>
-            <AddCircleIcon />
-          </Button>
+          <p>Name</p>
+        
+  
+          <br/>
+          <p>Log Out</p>
           
         </Grid>
         <Grid item xs={10} id="data-tbl">
-          <DataTable/>
+          <Tables/>
         </Grid>
 
       </Grid>
