b3f0a00a0d9f5a4f4606d6b10ac38e8384f5f09d
Ariel <aab171@outlook.com>
2021-12-07 13:22:40 -0500

    4: Merged App.js

--- bt-table/src/App.js
+++ bt-table/src/App.js
@@ -4,10 +4,23 @@ import Tables from './components/Tables';
 import { Grid } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Button } from '@material-ui/core';
+import Navbar from "./Components/Navbar/Navbar";
+import Homepage from "./homepage.js"
+import Signin from "./signin.js";
+import Signup from "./signup.js";
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      
+      <Navbar />
+
+      <Routes>
+        <Route path="/" exact  element={<Homepage />} />
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes >
       
       <Grid container spacing={0} id="entire-table" >
         <Grid item xs={2} id="panel" >
